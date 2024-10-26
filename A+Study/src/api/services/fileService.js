5dc00c39c926ff277d52a53602ea2896a57b0d4a
talaZayed2002 <137496459+talaZayed2002@users.noreply.github.com>
2024-10-26 10:11:57 +0200

    AWS commit_1 (file controller, service, router)

--- A+Study/src/api/services/fileService.js
+++ A+Study/src/api/services/fileService.js
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
-
 export const fileService = {
+    async upload(data){
+        return await prisma.file.create({            
+            data
+        });
+    },
     async updateFileDetails(fileId,data){
         return await prisma.file.update({
             where: { fileId: parseInt(fileId) },
@@ -12,16 +16,20 @@ export const fileService = {
         return await prisma.flashcard.findMany({
             where: { fileId : parseInt(fileId) }
         });
-    } ,
+    },
     async getKeytermsByFileId(fileId){
         return await prisma.keyterm.findMany({
             where: { fileId : parseInt(fileId) }
         });
-    } ,
+    },
     async getfilesByName(Name) {
         return await prisma.file.findMany({
             where: {fileName: { contains: Name} }            
         });
-    }
-
+    },
+    async deleteFile(id) {
+        return await prisma.file.delete({
+            where: { fileId: parseInt(id) }
+        });
+    },
 }
