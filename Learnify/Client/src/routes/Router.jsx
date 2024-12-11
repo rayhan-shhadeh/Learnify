08181ddb7263ac8ce003b070cb2ec528b3c413f6
talaZayed2002 <137496459+talaZayed2002@users.noreply.github.com>
2024-12-11 19:01:41 +0200

    routes

--- Learnify/Client/src/routes/Router.jsx
+++ Learnify/Client/src/routes/Router.jsx
@@ -2,18 +2,16 @@ import React, { lazy } from 'react';
 import { Navigate } from 'react-router-dom';
 import Loadable from '../layouts/full/shared/loadable/Loadable';
 
-/* Layouts */
+import Main from "../layouts/Main";
+import Home from "../views/Home";
+import Calender from "../views/Calender";
+
 const FullLayout = Loadable(lazy(() => import('../layouts/full/FullLayout')));
 const BlankLayout = Loadable(lazy(() => import('../layouts/blank/BlankLayout')));
 
-// /* Pages */
-// const Dashboard = Loadable(lazy(() => import('../views/dashboard/Dashboard')));
-const Home = Loadable(lazy(() => import('../views/Home'))); // Your Home page
+//const Home = Loadable(lazy(() => import('../views/Home'))); // Your Home page
 const HomePage = Loadable(lazy(() => import('../views/HomePage'))); // Your Home page
-// const Profile = Loadable(lazy(() => import('../views/Home'))); // Your Profile page
-// const SignUp = Loadable(lazy(() => import('../views/authentication/SignUp')));
-// const Login = Loadable(lazy(() => import('../views/authentication/Login')));
-// const Error = Loadable(lazy(() => import('../views/authentication/Error')));
+
 const Dashboard = Loadable(lazy(() => import('../views/dashboard/Dashboard')))
 const SamplePage = Loadable(lazy(() => import('../views/sample-page/SamplePage')))
 const Icons = Loadable(lazy(() => import('../views/icons/Icons')))
@@ -22,16 +20,15 @@ const Shadow = Loadable(lazy(() => import('../views/utilities/Shadow')))
 const Error = Loadable(lazy(() => import('../views/authentication/Error')));
 const Register = Loadable(lazy(() => import('../views/authentication/Register')));
 const Login = Loadable(lazy(() => import('../views/authentication/Login')));
-import Footer from '../layouts/full/header/Footer';
-//import { Home } from '@mui/icons-material';
+
 
 const Router = [
-  {
-    path: '/',
+  /*
+  {  path: '//',
     element: <FullLayout />,
     children: [
       { path: '/', element: <Navigate to="/Home" /> },
-      { path: '/Home', element: <Home /> },
+      { path: '/Home', element: <Home1 /> },
       { path: '/Homepage', element: <HomePage /> },
       { path: '/dashboard', exact: true, element: <Dashboard /> },
       { path: '/sample-page', exact: true, element: <SamplePage /> },
@@ -39,8 +36,9 @@ const Router = [
       { path: '/ui/typography', exact: true, element: <TypographyPage /> },
       { path: '/ui/shadow', exact: true, element: <Shadow /> },
       { path: '*', element: <Navigate to="/auth/404" /> },
-    ],
-  },
+    ]
+  }
+  */
   {
     path: '/auth',
     element: <BlankLayout />,
@@ -48,9 +46,30 @@ const Router = [
       { path: '404', element: <Error /> },
       { path: '/auth/register', element: <Register /> },
       { path: '/auth/login', element: <Login /> },
+      { path: '/auth/logout', element: <Login /> },
       { path: '*', element: <Navigate to="/auth/404" /> },
     ],
   },
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/calendar",
+        element: <Calender></Calender>,
+      }   
+    ]
+  },{
+    path: '/',
+    element: <FullLayout></FullLayout>,
+    children: [
+    { path: '/dashboard', exact: true, element: <Dashboard /> }
+    ]
+  }
 ];
 
 export default Router;
