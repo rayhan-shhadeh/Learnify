6367e2e50d866d9d479b13d3fcfec74bb1a318fa
talaZayed2002 <137496459+talaZayed2002@users.noreply.github.com>
2024-12-11 19:07:42 +0200

    dashboard components

--- Learnify/Client/src/views/dashboard/components/YearlyBreakup.jsx
+++ Learnify/Client/src/views/dashboard/components/YearlyBreakup.jsx
@@ -3,6 +3,8 @@ import Chart from 'react-apexcharts';
 import { useTheme } from '@mui/material/styles';
 import { Grid, Stack, Typography, Avatar } from '@mui/material';
 import { IconArrowUpLeft } from '@tabler/icons-react';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+
 
 import DashboardCard from '../../../components/shared/DashboardCard';
 
@@ -59,57 +61,34 @@ const YearlyBreakup = () => {
       },
     ],
   };
-  const seriescolumnchart = [38, 40, 25];
-
   return (
-    <DashboardCard title="Yearly Breakup">
-      <Grid container spacing={3}>
-        {/* column */}
-        <Grid item xs={7} sm={7}>
-          <Typography variant="h3" fontWeight="700">
-            $36,358
-          </Typography>
-          <Stack direction="row" spacing={1} mt={1} alignItems="center">
-            <Avatar sx={{ bgcolor: successlight, width: 27, height: 27 }}>
-              <IconArrowUpLeft width={20} color="#39B69A" />
-            </Avatar>
-            <Typography variant="subtitle2" fontWeight="600">
-              +9%
-            </Typography>
-            <Typography variant="subtitle2" color="textSecondary">
-              last year
-            </Typography>
-          </Stack>
-          <Stack spacing={3} mt={5} direction="row">
-            <Stack direction="row" spacing={1} alignItems="center">
-              <Avatar
-                sx={{ width: 9, height: 9, bgcolor: primary, svg: { display: 'none' } }}
-              ></Avatar>
-              <Typography variant="subtitle2" color="textSecondary">
-                2022
-              </Typography>
-            </Stack>
-            <Stack direction="row" spacing={1} alignItems="center">
-              <Avatar
-                sx={{ width: 9, height: 9, bgcolor: primarylight, svg: { display: 'none' } }}
-              ></Avatar>
-              <Typography variant="subtitle2" color="textSecondary">
-                2023
-              </Typography>
-            </Stack>
-          </Stack>
-        </Grid>
-        {/* column */}
-        <Grid item xs={5} sm={5}>
-          <Chart
-            options={optionscolumnchart}
-            series={seriescolumnchart}
-            type="donut"
-            height="150px"
-          />
-        </Grid>
-      </Grid>
-    </DashboardCard>
+<DashboardCard title="Average Habit Streak">
+  <Grid container spacing={3}>
+    {/* Column 1: Streak Information */}
+    <Grid item xs={8} sm={8}>
+      <Typography variant="h3" fontWeight="700">
+        5 Days
+      </Typography>
+      <Stack direction="row" spacing={1} mt={1} alignItems="center">
+        <Avatar sx={{ bgcolor: successlight, width: 27, height: 27 }}>
+          <IconArrowUpLeft width={20} color="#39B69A" />
+        </Avatar>
+        <Typography variant="subtitle2" fontWeight="600">
+          +2 Days
+        </Typography>
+        <Typography variant="subtitle2" color="textSecondary">
+          from last week
+        </Typography>
+      </Stack>
+    </Grid>
+    {/* Column 2: fire */}
+    <Grid item xs={4} sm={4}>
+      <Avatar sx={{ bgcolor: "#ffffff", width: 60, height: 60 }}>
+        <LocalFireDepartmentIcon sx={{ fontSize: 80, color: "orange" }} />
+      </Avatar>
+    </Grid>
+  </Grid>
+</DashboardCard>
   );
 };
 
