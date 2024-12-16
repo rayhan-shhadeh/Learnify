41cb12f2d717891f07525a43952c36bb0da07476
rayhan-shhadeh <re7an.ma7moud@gmail.com>
2024-12-16 15:29:27 +0200

    Authentication in signin screen

--- Learnify/mobileapp/aplusmobile/api/axois.js
+++ Learnify/mobileapp/aplusmobile/api/axois.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+//192.168.68.58
 const API = axios.create({
   baseURL: 'http://localhost:8080/api/', // Replace with your backend URL
   withCredentials: true, // Enable cookies for CORS
@@ -7,7 +7,7 @@ const API = axios.create({
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  'Authorization': 'Bearer '+localStorage.getItem('token')
+
 }});
 
 
