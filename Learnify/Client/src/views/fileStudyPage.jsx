ba325b900836263668e3a5e2214902ba910547e9
talaZayed2002 <137496459+talaZayed2002@users.noreply.github.com>
2024-12-24 21:40:56 +0200

    (study-practice-quiz) views and components  + flashcard component

---
+++ Learnify/Client/src/views/fileStudyPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FileStudy from "../Components/fileComponents/fileStudyKF";
+import "../CSS/fileStudy.css"
+
+const FileStudyPage = () => {
+    return (
+      <div>
+        <FileStudy></FileStudy>
+      </div>
+    );
+  };
+  
+  export default FileStudyPage;
