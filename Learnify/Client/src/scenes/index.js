da85f58fe7e41fcc8fd8473446bb0f7ae1fdfd8b
RayhanShadeh <re7an.ma7moud@gmail.com>
2025-01-20 03:47:03 +0200

    Dashboard - scenes

---
+++ Learnify/Client/src/scenes/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./layout/navbar";
+import SideBar from "./layout/sidebar";
+import Dashboard from "./dashboard";
+import Team from "./team";
+import Invoices from "./invoices";
+import Contacts from "./contacts";
+import Form from "./form";
+import Calendar from "./calendar";
+import Bar from "./bar";
+import Line from "./line";
+import Pie from "./pie";
+import Stream from "./stream";
+import FAQ from "./faq";
+
+export {
+  Navbar,
+  SideBar,
+  Dashboard,
+  Team,
+  Invoices,
+  Contacts,
+  Form,
+  Calendar,
+  Bar,
+  Line,
+  Pie,
+  Stream,
+  FAQ,
+};
