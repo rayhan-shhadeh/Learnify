b8a302f4af9befeaf9259d3446bb5c93ab48f1a6
RayhanShadeh <re7an.ma7moud@gmail.com>
2025-01-29 06:07:31 +0200

    navigation update

--- Learnify/mobileapp/aplusmobile/app/App.tsx
+++ Learnify/mobileapp/aplusmobile/app/App.tsx
@@ -3,12 +3,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import IndexScreen from "./index"; // Your IndexScreen component
 import FilesScreen from "./(tabs)/FilesScreen";
-import PdfScreen from "./(tabs)/Files/PdfScreen";
-import Habits from "./(tabs)/Habits/Habits";
-import { StreakProvider } from "./(tabs)/hooks/StreakContext";
 import HabitsScreen from "./(tabs)/Habits/HabitsScreen";
 import StreakFireTest from "./(tabs)/streak/SreakFireTest";
 import TestComponent from "./(tabs)/TestComponent";
+import { StreakProvider } from "./(tabs)/hooks/StreakContext";
 
 const Stack = createStackNavigator();
 
@@ -16,32 +14,15 @@ const App = () => {
   return (
     <StreakProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="IndexScreen">
-          <Stack.Screen
-            name="IndexScreen"
-            component={IndexScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="FilesScreen"
-            component={FilesScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="HabitsScreen"
-            component={HabitsScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="StrakFireTest"
-            component={StreakFireTest}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="TestComponent"
-            component={TestComponent}
-            options={{ headerShown: false }}
-          />
+        <Stack.Navigator
+          initialRouteName="IndexScreen"
+          screenOptions={{ headerShown: true }}
+        >
+          <Stack.Screen name="IndexScreen" component={IndexScreen} />
+          <Stack.Screen name="FilesScreen" component={FilesScreen} />
+          <Stack.Screen name="HabitsScreen" component={HabitsScreen} />
+          <Stack.Screen name="StrakFireTest" component={StreakFireTest} />
+          <Stack.Screen name="TestComponent" component={TestComponent} />
         </Stack.Navigator>
       </NavigationContainer>
     </StreakProvider>
