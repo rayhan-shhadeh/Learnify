d41eca178a54b27c7f5c0960996c1ff76e2c9987
RayhanShadeh <re7an.ma7moud@gmail.com>
2025-01-29 06:06:49 +0200

    update, delete courses context and picker

--- Learnify/mobileapp/aplusmobile/app/_layout.tsx
+++ Learnify/mobileapp/aplusmobile/app/_layout.tsx
@@ -8,7 +8,6 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import { CoursesProvider } from "./(tabs)/hooks/CoursesContext";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
