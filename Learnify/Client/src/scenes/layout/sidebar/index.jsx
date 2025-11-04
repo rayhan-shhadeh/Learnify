6b9833b77b117b9f24517242d373dc8cffea4db1
RayhanShadeh <re7an.ma7moud@gmail.com>
2025-11-04 18:00:49 +0200

    fix logo import path in sidebar component

--- Learnify/Client/src/scenes/layout/sidebar/index.jsx
+++ Learnify/Client/src/scenes/layout/sidebar/index.jsx
@@ -19,7 +19,7 @@ import {
   WavesOutlined,
 } from "@mui/icons-material";
 import avatar from "../../../assets/images/profile/girl.png";
-import logo from "../../../assets/A+.png";
+import logo from "../../../assets/A+1.svg";
 import Item from "./Item";
 import { ToggledContext } from "../../../Toggled";
 
